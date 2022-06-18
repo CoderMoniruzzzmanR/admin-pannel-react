3a884800186dd644897f11490a8a403d3fee4463
CoderMoniruzzzmanR <moniruzzaman.eng.info@gmail.com>
2022-06-19 02:20:46 +0600

    sidebar design

--- src/layouts/Sidebar.js
+++ src/layouts/Sidebar.js
@@ -4,9 +4,13 @@ import SimpleBar from 'simplebar-react';
 import { CSSTransition } from 'react-transition-group';
 import { ARouter } from "../routes";
 import { Nav, Badge, Button, Dropdown, Accordion, Navbar } from 'react-bootstrap';
-
-import LogoImg from "../assets/img/logo4.svg";
+import { FiHome, FiBox, FiCircle, FiChevronRight, FiUser } from "react-icons/fi";
+import { BsChevronExpand } from "react-icons/bs";
+import { AiOutlineTable } from "react-icons/ai";
 import Logo from '../components/Logo';
+import LogoImg from "../assets/img/logo4.svg";
+import UserImg from "../assets/img/user.jpg";
+
 
 
 function Sidebar() {
@@ -24,13 +28,14 @@ function Sidebar() {
    return (
      <Accordion as={Nav.Item} defaultActiveKey={defaultKey}>
        <Accordion.Item eventKey={eventKey}>
-         <Accordion.Button as={Nav.Link} className="d-flex justify-content-between align-items-center">
+         <Accordion.Button as={Nav.Link}>
            <span>
-             <span className="sidebar-icon"> </span>
-             <span className="sidebar-text">{title}</span>
+             <span className="group-icon">{icon}</span>
+             <span className="group-text">{title}</span>
+             <span className="arrow"><FiChevronRight className="icon"/></span>
            </span>
          </Accordion.Button>
-         <Accordion.Body className="multi-level">
+         <Accordion.Body className="multi-menu">
            <Nav className="flex-column">
              {children}
            </Nav>
@@ -42,7 +47,7 @@ function Sidebar() {
 
  const NavItem = (props) => {
 
-   const { title, link, external, target, icon, image, badgeText, badgeBg = "secondary", badgeColor = "primary" } = props;
+   const { title, link, external, target, icon, badgeText, badgeBg = "secondary", badgeColor = "primary" } = props;
 
    const classNames = badgeText ? "d-flex justify-content-start align-items-center justify-content-between" : "";
    const navItemClassName = link === pathname ? "active" : "";
@@ -52,7 +57,7 @@ function Sidebar() {
      <Nav.Item className={navItemClassName} onClick={() => setShow(false)}>
        <Nav.Link {...linkProps} target={target} className={classNames}>
          <span>
-           <span>#</span>
+           <span>{icon}</span>
            <span className="sidebar-text">{title}</span>
          </span>
          {badgeText ? (
@@ -98,39 +103,49 @@ function Sidebar() {
                   </Link>
                </div>
             </div>
-            <div className="sidebar-inner px-4 pt-3">
+            {/* Sidebar Footer*/}
+            <button className="sidebar-navbar-user">
+               <div className="user">
+                  <img src={UserImg} alt="user.jpg" className="img-fluid"/>
+                  <div className="user-info">
+                     <h3>Paul Melone</h3>
+                     <span>Python Dev</span>
+                  </div>
+               </div>
+               <span className="expand-icon"><BsChevronExpand/></span>
+            </button>
+            <div className="sidebar-inner">
                {/* Sidebar Menu*/}
-               <Nav className="flex-column pt-3 pt-md-0">
+               <Nav className="main-menu">
 
-                  <NavItem title="Overview" link={ARouter.DashboardOverview.path} icon="$" />
+                  <NavItem title="Dashboard" link={ARouter.DashboardOverview.path} icon={<FiHome className="icon"/>}/>
                  
-                  <CollapsableNavItem eventKey="tables/" title="Tables" icon="$">
-                     <NavItem title="Bootstrap Table" link={ARouter.BootstrapTables.path} />
+                  <CollapsableNavItem eventKey="tables/" title="Tables" icon={<AiOutlineTable className="icon"/>}>
+                     <NavItem title="Bootstrap Table" link={ARouter.BootstrapTables.path} icon={<FiCircle className="multi-menu-icon"/>}/>
                   </CollapsableNavItem>
                  
-                  <NavItem external title="Plugins" link="https://demo.themesberg.com/volt-pro-react/#/plugins/datatable" target="_blank" badgeText="Pro" icon="$" />
+                  {/* <NavItem external title="Plugins" link="https://demo.themesberg.com/volt-pro-react/#/plugins/datatable" target="_blank" badgeText="Pro" icon="$" /> */}
                  
-                  <Dropdown.Divider className="my-3 border-indigo" />
-                  <CollapsableNavItem eventKey="documentation/" title="Getting Started" icon="$">
-                     <NavItem title="Overview" link={ARouter.DocsOverview.path} />
-                     <NavItem title="Download" link={ARouter.DocsDownload.path} />
-                     <NavItem title="Quick Start" link={ARouter.DocsQuickStart.path} />
+                  {/* <Dropdown.Divider className="my-3 border-indigo" /> */}
+                  <CollapsableNavItem eventKey="documentation/" title="Getting Started" icon={<FiBox className="icon"/>}>
+                     <NavItem title="Overview" link={ARouter.DocsOverview.path} icon={<FiCircle className="multi-menu-icon"/>}/>
+                     <NavItem title="Download" link={ARouter.DocsDownload.path} icon={<FiCircle className="multi-menu-icon"/>}/>
+                     <NavItem title="Quick Start" link={ARouter.DocsQuickStart.path} icon={<FiCircle className="multi-menu-icon"/>}/>
                   </CollapsableNavItem>
                  
-                  <CollapsableNavItem eventKey="user/" title="User Started" icon="$">
-                     <NavItem title="User" link={ARouter.UserList.path} />
+                  <CollapsableNavItem eventKey="user/" title="User Management" icon={<FiUser className="icon"/>}>
+                     <NavItem title="User" link={ARouter.UserList.path} icon={<FiCircle className="multi-menu-icon"/>}/>
                   </CollapsableNavItem>
                  
-                  <CollapsableNavItem eventKey="components/" title="Components" icon="$">
-                     <NavItem title="Accordion" link={ARouter.Accordions.path} />
-                     <NavItem title="Alerts" link={ARouter.Alerts.path} />
-                     <NavItem title="Badges" link={ARouter.Badges.path} />
+                  <CollapsableNavItem eventKey="components/" title="Components" icon={<FiBox className="icon"/>}>
+                     <NavItem title="Accordion" link={ARouter.Accordions.path} icon={<FiCircle className="multi-menu-icon"/>}/>
+                     <NavItem title="Alerts" link={ARouter.Alerts.path} icon={<FiCircle className="multi-menu-icon"/>}/>
+                     <NavItem title="Badges" link={ARouter.Badges.path} icon={<FiCircle className="multi-menu-icon"/>}/>
                   </CollapsableNavItem>
                   
                </Nav>
             </div>
-            {/* Sidebar Footer*/}
-            <div className="sidebar-navbar-user">footer</div>
+            
          </SimpleBar>
       </CSSTransition>
    </>
