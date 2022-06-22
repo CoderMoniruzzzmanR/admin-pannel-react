5b14b5c2c327248d2b7b4086f173ccf604995fcb
CoderMoniruzzzmanR <moniruzzaman.eng.info@gmail.com>
2022-06-23 01:49:08 +0600

    navbar notification working

--- src/layouts/Navbar/NavNotification.js
+++ src/layouts/Navbar/NavNotification.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Icon from '../../shared/Icon';
 import {Dropdown} from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 function NavNotification() {
   return (
@@ -10,10 +11,23 @@ function NavNotification() {
             <Icon name="notification" className="icon"/>
          </Dropdown.Toggle>
          <Dropdown.Menu>
-            <Dropdown.Item href="#/"><span className="icon"><Icon name="check-square"/></span>New Task</Dropdown.Item>
-            <Dropdown.Item href="#/"><span className="icon"><Icon name="user"/></span>New User</Dropdown.Item>
-            <Dropdown.Item href="#/"><span className="icon"><Icon name="store"/></span>New product</Dropdown.Item>
-            <Dropdown.Item href="#/"><span className="icon"><Icon name="calendar"/></span>New event</Dropdown.Item>
+            <div className="drop-heading">
+               <h4>Notifications</h4>
+               <div><Link to="/" className="badge-primary">6 New</Link></div>
+            </div>
+            <div className="drop-items">
+               <Dropdown.Item href="#/">
+               </Dropdown.Item>
+               <Dropdown.Item href="#/"><span className="icon"><Icon name="user"/></span>New User</Dropdown.Item>
+               <Dropdown.Item href="#/"><span className="icon"><Icon name="store"/></span>New product</Dropdown.Item>
+               <Dropdown.Item href="#/"><span className="icon"><Icon name="calendar"/></span>New event</Dropdown.Item>
+            </div>
+            <div className="drop-footer">
+               <Dropdown.Item href="#/" className="btn-basic">
+                  <span>View more<span className="icon"><Icon name="arrow-right"/></span></span>
+               </Dropdown.Item>
+            </div>
+            
          </Dropdown.Menu>
       </Dropdown>
     </li>
